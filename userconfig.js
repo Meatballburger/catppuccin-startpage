f0c41539858d8523569bb0a8619de41eb4c95a32
Meatballburger <e_give@proton.me>
2025-08-13 17:23:34 -0500

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -262,7 +262,7 @@ const default_configuration = {
             {
               name: "itch.io",
               url: "https://itch.io/games",
-              icon: "https://static.itch.io/images/itchio-textless-black.svg",
+              icon: "brand-itch io",
               icon_color: palette.red,
             },
             {
