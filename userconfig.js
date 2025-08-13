554d64b9b7c2899dc746438e7cf4f74108e53b15
Meatballburger <e_give@proton.me>
2025-08-13 17:25:32 -0500

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -268,7 +268,7 @@ const default_configuration = {
             {
               name: "gog",
               url: "https://www.gog.com/en/",
-              icon: "brand-gog com",
+              icon: "brand-GOG",
               icon_color: palette.blue,
             },
           ],
